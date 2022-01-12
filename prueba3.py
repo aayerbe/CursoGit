7b00f244e9a32502a0c0ed2bc9cbc31a1461acad
Alex <alexayerbe91@gmail.com>
2022-01-12 09:51:34 +0100

    Comienzo

---
+++ prueba3.py
@@ -0,0 +1,18 @@
+email=input("Introduzca su dirección de correo ")
+def comprobar(palabra):
+	if palabra.count("@")==1 and palabra.endswith("@")==False and palabra.startswith("@")==False:
+		print("Es correcto")
+	elif palabra.count("@")!=1: 
+		print("Numero de @ incorrecto")
+	elif palabra.endswith("@")==True:
+		print("Termina en @")
+	elif palabra.startswith("@")==True:
+		print("Empieza por @")		
+	else:
+		print("Es incorrecto")
+comprobar(email)
+		
+
+	
+
+		
