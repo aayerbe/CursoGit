0ba249c18d7ad2b9db1e6346edfdffd818b222bc
aayerbe <95773951+aayerbe@users.noreply.github.com>
2022-01-13 15:29:07 +0100

    Update prueba3.py

--- prueba3.py
+++ prueba3.py
@@ -1,7 +1,7 @@
 email=input("Introduzca su dirección de correo ")
 def comprobar(palabra):
 	if palabra.count("@")==1 and palabra.endswith("@")==False and palabra.startswith("@")==False:
-		print("Es correctoooo")
+		print("Es correcto(Actualizacion desde github")
 	elif palabra.count("@")!=1: 
 		print("Numero de @ incorrecto")
 	elif palabra.endswith("@")==True:
